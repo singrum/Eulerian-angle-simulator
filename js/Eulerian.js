4f664f93343a412a5b7fad26f4fcce838931490d
Singrum <103807515+singrum@users.noreply.github.com>
2022-11-23 22:04:54 +0900

    Add files via upload

--- js/Eulerian.js
+++ js/Eulerian.js
@@ -1,5 +1,5 @@
 
-let scene, camera, renderer;
+let scene, camera, renderer, controls;
 
 
 
@@ -20,8 +20,6 @@ function init(){
     camera.position.x = 30;
     camera.position.y = 30;
     camera.position.z = 30;
-    camera.up = new THREE.Vector3(0,0,1);
-    camera.lookAt(scene.position);
     scene.add(camera);
     
     //ligths
@@ -32,45 +30,46 @@ function init(){
     
     //fixed axes
     let axes_f = new THREE.AxesHelper(20);
-    axes_f.setColors("black","black","black");
+    axes_f.setColors("red","green","blue");
     scene.add(axes_f);
-
-    //rotated axes
-    let axes_r = new THREE.AxesHelper(20);
-    axes_r.setColors("red","red","red");
-    scene.add(axes_r);
 
     //object(box)
     let box = new THREE.Mesh(
         new THREE.BoxGeometry(3,5,7),
         new THREE.MeshLambertMaterial({color : 0xdddddd})
     )
-    box.position.x = 1.5;
-    box.position.y = 2.5;
-    box.position.z = 3.5;
-    box.castShadow = true;
+    box.matrixAutoUpdate = false;
     scene.add(box)
+    console.log(box)
+
 
     //gui 생성
-    let controls = new function(){
-        this.EulerX = 0;
-        this.EulerY = 0;
-        this.EulerZ = 0;
+    let guicontrols = new function(){
+        this.Euler1 = 0;
+        this.Euler2 = 0;
+        this.Euler3 = 0;
     }
     let gui = new dat.GUI();
-    gui.add(controls, "EulerX", -2 * Math.PI, 2 * Math.PI);
-    gui.add(controls, "EulerY", -2 * Math.PI, 2 * Math.PI);
-    gui.add(controls, "EulerZ", -2 * Math.PI, 2 * Math.PI);
+    gui.add(guicontrols, "Euler1", -2 * Math.PI, 2 * Math.PI);
+    gui.add(guicontrols, "Euler2", -2 * Math.PI, 2 * Math.PI);
+    gui.add(guicontrols, "Euler3", -2 * Math.PI, 2 * Math.PI);
 
     function renderScene(){
-        box.rotation.set(controls.EulerX,controls.EulerY,controls.EulerZ)
-        axes_r.rotation.set(controls.EulerX,controls.EulerY,controls.EulerZ);
+        box.matrix = new THREE.Matrix4().makeRotationX(guicontrols.Euler3).multiply(
+            new THREE.Matrix4().makeRotationY(guicontrols.Euler2).multiply(
+                new THREE.Matrix4().makeRotationX(guicontrols.Euler1).multiply(
+                    new THREE.Matrix4().makeTranslation(1.5, 2.5, 3.5)
+                )
+            )
+        );
+        controls.update();
         requestAnimationFrame(renderScene);
         renderer.render(scene, camera);
     }
 
     //renderer
     renderer = new THREE.WebGLRenderer();
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
     renderer.setClearColor(0xEEEEEE);
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.shadowMap.enabled = true;
